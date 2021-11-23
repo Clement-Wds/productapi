fe100b27920a3c6fa1a5bb8c6e715fd8afed883c
clement-wds <clementwalshdeserrant@outlook.com>
2021-11-23 13:58:38 +0100

    Delete Product

--- src/main/java/com/ynovb3/productapi/controller/ProductController.java
+++ src/main/java/com/ynovb3/productapi/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,6 +38,11 @@ public class ProductController {
 	@PostMapping("/product")
 	public void addProduct(@RequestBody Product product) {
 		productRepository.save(product);
+	}
+	
+	@DeleteMapping("/product/{id}")
+	public void deleteProduct(@PathVariable("id") Integer id) {
+		productRepository.deleteById(id);
 	}
 
 }
