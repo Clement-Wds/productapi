9dc79cc7506ad3ccc4dbea2e25d1dcf58b29ed30
clement-wds <clementwalshdeserrant@outlook.com>
2021-11-23 15:05:00 +0100

    Native query for FindByName

--- src/main/java/com/ynovb3/productapi/repository/ProductRepository.java
+++ src/main/java/com/ynovb3/productapi/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.ynovb3.productapi.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ynovb3.productapi.model.Product;
@@ -8,6 +10,11 @@ import com.ynovb3.productapi.model.Product;
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
+	//Derived query (requête dérivée)
 	public Iterable<Product> findByName(String name);
+	
+	//Native query
+	@Query(value = "SELECT * FROM products WHERE name = :name", nativeQuery = true)
+	public Iterable<Product> findByNameNative(@Param("name") String name);
 	
 }
