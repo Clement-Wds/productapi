1d89f3e2b5cf57558f5f34fa666e3ec27ea5f279
clement-wds <clementwalshdeserrant@outlook.com>
2021-11-23 14:04:16 +0100

    Update Product

--- src/main/java/com/ynovb3/productapi/controller/ProductController.java
+++ src/main/java/com/ynovb3/productapi/controller/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,6 +44,11 @@ public class ProductController {
 	@DeleteMapping("/product/{id}")
 	public void deleteProduct(@PathVariable("id") Integer id) {
 		productRepository.deleteById(id);
+	}
+	
+	@PutMapping("/product")
+	public Product replaceProduct(@RequestBody Product product) {
+		return productRepository.save(product);
 	}
 
 }
