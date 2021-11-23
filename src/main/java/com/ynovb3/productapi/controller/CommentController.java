d92b34f15d04761a92d82cfadfbee1b083a5b0f7
clement-wds <clementwalshdeserrant@outlook.com>
2021-11-23 17:18:40 +0100

    Comment Delete + Update Comment Service

--- src/main/java/com/ynovb3/productapi/controller/CommentController.java
+++ src/main/java/com/ynovb3/productapi/controller/CommentController.java
@@ -1,6 +1,7 @@
 package com.ynovb3.productapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ynovb3.productapi.model.Comment;
 import com.ynovb3.productapi.model.Product;
+import com.ynovb3.productapi.service.CommentService;
 import com.ynovb3.productapi.service.ProductService;
 
 @RestController
@@ -15,9 +17,12 @@ public class CommentController {
 	
 	@Autowired
 	private ProductService productService;
+	
+	@Autowired
+	private CommentService commentService;
 			
 	@PostMapping("/comment/{product_id}")
-	public Product addComment(@RequestBody Comment comment, @PathVariable Integer product_id) {
+	public Product addComment(@RequestBody Comment comment, @PathVariable("product_id") Integer product_id) {
 
 		Product existingProduct = productService.getProduct(product_id).get();
 
@@ -25,6 +30,11 @@ public class CommentController {
 		
 		return productService.upsert(existingProduct);
 		
+	}
+	
+	@DeleteMapping("/comment/{id}")
+	public void deleteComment(@PathVariable("id") Integer id) {
+		commentService.deleteComment(id);
 	}
 	
 }
--- src/main/java/com/ynovb3/productapi/service/CommentService.java
+++ src/main/java/com/ynovb3/productapi/service/CommentService.java
@@ -2,11 +2,20 @@ package com.ynovb3.productapi.service;
 
 import org.springframework.stereotype.Service;
 
+import com.ynovb3.productapi.model.Comment;
 import com.ynovb3.productapi.repository.CommentRepository;
 
 @Service
 public class CommentService {
 	
 	private CommentRepository commentRepository;
+	
+	public Comment upsert(Comment product) {
+        return commentRepository.save(product);
+    }
+
+    public void deleteComment(Integer id) {
+        commentRepository.deleteById(id);
+    }
 
 }
