414548fa68c919f6fa4173f2e3695dfe8ea0032c
clement-wds <clementwalshdeserrant@outlook.com>
2021-11-23 11:16:05 +0100

    Product Model

---
+++ src/main/java/com/ynovb3/productapi/model/Product.java
@@ -0,0 +1,51 @@
+package com.ynovb3.productapi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //Clé Primaire - Auto incrémentation
+	private int id;
+	
+	private String name;
+	private String description;
+	private int cost;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public int getCost() {
+		return cost;
+	}
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+	
+	
+
+}
