75a214c913ab4a316ae43f8dd5cbdce843890e65
clement-wds <clementwalshdeserrant@outlook.com>
2021-11-23 12:44:04 +0100

    Post Product

--- src/main/java/com/ynovb3/productapi/controller/ProductController.java
+++ src/main/java/com/ynovb3/productapi/controller/ProductController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ynovb3.productapi.model.Product;
@@ -30,6 +32,11 @@ public class ProductController {
 			return new ResponseEntity<Product>(p.get(), HttpStatus.OK);
 		}
 		return new ResponseEntity<Product>(HttpStatus.NOT_FOUND);//Return 404
+	}
+	
+	@PostMapping("/product")
+	public void addProduct(@RequestBody Product product) {
+		productRepository.save(product);
 	}
 
 }
