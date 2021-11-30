5e6e820d7c84c8f8719e87fd51a86e8d2350695e
clement-wds <clementwalshdeserrant@outlook.com>
2021-11-30 10:32:39 +0100

    ManyToMany Category Product

--- src/main/java/com/ynovb3/productapi/model/Category.java
+++ src/main/java/com/ynovb3/productapi/model/Category.java
@@ -1,10 +1,18 @@
 package com.ynovb3.productapi.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -17,6 +25,33 @@ public class Category {
 	private Integer categoryId;
 	
 	private String name;
+	
+	@ManyToMany(
+			fetch = FetchType.LAZY, //Performance
+			cascade = {
+					CascadeType.PERSIST, //création
+					CascadeType.MERGE //modification
+			}
+			)
+	
+	@JoinTable(
+			//nom de la table de jointure
+			name = "category_product", 
+			//clé étrangère dans la table de jointure correspondant à la clé primaire de la classe courante category
+			joinColumns = @JoinColumn(name = "category_id"), 
+			//clé étrangère dans la table de jointure coorespondant à la clé primaire de la classe en relation product
+			inverseJoinColumns = @JoinColumn(name = "product_id")
+			)
+	
+	private List<Product> products = new ArrayList<>();
+	
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
 
 	public Integer getCategoryId() {
 		return categoryId;
