e285c6527eb72098da2f36cf0043e5c5a2e7062f
clement-wds <clementwalshdeserrant@outlook.com>
2021-12-07 11:58:02 +0100

    Update SecurityConfig

--- src/main/java/com/ynovb3/productapi/configuration/SecurityConfig.java
+++ src/main/java/com/ynovb3/productapi/configuration/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ynovb3.productapi.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,8 +14,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
-//	@Autowired
-//	private CustomUserDetailsService userDetailsService;
+	@Autowired
+	private CustomUserDetailsService userDetailsService;
 	
 	protected void configure(HttpSecurity http) throws Exception {
 		
@@ -37,7 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		auth.inMemoryAuthentication().passwordEncoder(passwordEncoder()).withUser("clement").password(encodedPassword).roles("USER");
 		
-		auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 	}
 	
 }
